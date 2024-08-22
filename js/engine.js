1595ea7c4837818dc6f78157264ead836c27ef7d
Jonas Nettli <jonas.nettli@gmail.com>
2024-08-22 09:09:30 +0200

    Got demo-images and started drawing images to the canvas

--- js/engine.js
+++ js/engine.js
@@ -21,6 +21,15 @@ ${this.#message}`
 
     )
   }
+
+  get hitPoints() {
+    return this.#hitPoints;
+  }
+
+  set hitPoints(hp) {
+    this.#hitPoints = hp;
+    console.log(`HP is now: ${this.#hitPoints}`);
+  }
 }
 
 const goblin = new CharacterStats(10, 10, 1, 1, "10 gold", "Ugly green guy");
@@ -28,3 +37,5 @@ const imp = new CharacterStats(5, 5, 2, 2, "5 gold", "Evil little red guy");
 const evilRabbit = new CharacterStats(999, 999, 999, 999, "Holy Grail", "Big mistake");
 
 goblin.monsterExamine();
+
+goblin.hitPoints = 5;
