f1c195f0deeef227bdaded09f10196d98031d9fe
Jonas Nettli <jonas.nettli@gmail.com>
2024-08-22 09:29:23 +0200

    Added character with shadow to map

--- js/overworld.js
+++ js/overworld.js
@@ -11,5 +11,23 @@ class Overworld {
             this.ctx.drawImage(image, 0, 0);
         };
         image.src = 'img/maps/DemoLower.png';
+
+        const shadow = new Image();
+        shadow.onload = () => {
+            this.ctx.drawImage(
+                shadow, 0, 0, 32, 32, x * 16, y * 16, 32, 32
+            );
+        };
+        shadow.src = 'img/char/shadow.png';
+
+        const x = .5;
+        const y = 3;
+        const hero = new Image();
+        hero.onload = () => {
+            this.ctx.drawImage(
+                hero, 0, 0, 32, 32, x * 16, y * 16, 32, 32
+            );
+        };
+        hero.src = 'img/char/people/hero.png';
     }
 }
